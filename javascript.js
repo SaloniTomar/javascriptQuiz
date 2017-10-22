0b5d9187b63f01abab2af4be8e7506667fdb99b0
SaloniTomar <30462430+SaloniTomar@users.noreply.github.com>
2017-10-23 00:04:44 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-localStorage.clear();
 localStorage.setItem("ques", "1. Inside which HTML element do we put the JavaScript?,2. Where is the correct place to insert a JavaScript?,3. How do you create a function in JavaScript?,4. How to write an IF statement in JavaScript?,5. How can you add a comment in a JavaScript?");
                      
 localStorage.setItem("options", "javascript,scripting,script,js;In the body,Head, Body or head, Nowhere;function:myFunction(),function myFunction,function=myFunction,function = new myFunction;if i == 5 then,if (i == 5),if i = 5, if i = 5 then;<--This is a comment-->,'This is a comment,//This is a comment,<--This is a comment");
@@ -7,13 +6,21 @@ localStorage.setItem("correct", "3,3,2,2,3");
 
 var index=-1, score=0;
 
-function checkUser(){
+function continueQuiz(){
     var name= localStorage.getItem("name");
-    if(name){
-        document.getElementById("userData").style.display="none";
-        document.getElementById("content").style.display="block";
-        setContent();
+    var indexCurrent = localStorage.getItem(name+"Attempted").split(",");
+    index=indexCurrent[indexCurrent.length-1];
+    if(index > 4){
+        showResult();
     }
+    var correctAnswers=localStorage.getItem(name+"Correct");
+    if(correctAnswers){
+        var scoreCurrent =correctAnswers.split(",");
+        score=scoreCurrent.length;
+    }
+    document.getElementById("userData").style.display="none";
+    document.getElementById("content").style.display="block";
+    setContent();
 }
 
 function submitUsername(){
@@ -23,10 +30,17 @@ function submitUsername(){
             alert("Please enter your name");
         }
     else{
-        localStorage.setItem("name", name);
-        document.getElementById("userData").style.display="none";
-        document.getElementById("content").style.display="block";
-        setContent();
+        if(name===localStorage.getItem("name")){
+            alert(name);
+            continueQuiz();
+        }
+        else{
+            alert(name+"avn");
+            localStorage.setItem("name", name);
+            document.getElementById("userData").style.display="none";
+            document.getElementById("content").style.display="block";
+            setContent();
+        }
     }
 }
 function storeAnswer(ans){
@@ -58,18 +72,38 @@ function setContent(){
         document.getElementById("content").innerHTML+="<button id='submit2' onclick=showResult()>Submit</button>";
     }
 }
+
 function checkAnswer(ans){
     var correctAns=localStorage.getItem("correct").split(",")[index];
+    var name= localStorage.getItem("name");
     if(ans==correctAns){
        score++;   
+    var addedAnswers = localStorage.getItem(name+"Correct");
+    if (addedAnswers) {
+        localStorage.setItem(name+"Correct", addedAnswers + ',' + ans);
+    } else {
+        localStorage.setItem(name+"Correct", ans);
+    } 
     }
     var quesNum= index.toString(10);
-    localStorage.setItem(quesNum, ans);
+    var addedQuestions = localStorage.getItem(name+"Attempted");
+    if (addedQuestions) {
+        localStorage.setItem(name+"Attempted", addedQuestions + ',' + index);
+    } else {
+        localStorage.setItem(name+"Attempted", index);
+    }
 }
+
 function showResult(){
-     var answer=localStorage.getItem("selectedOption");
+    var answer=localStorage.getItem("selectedOption");
     checkAnswer(answer);  
     var name= localStorage.getItem("name");
+    var correctAnswers=localStorage.getItem(name+"Correct");
+    if(correctAnswers){
+        var scoreCurrent =correctAnswers.split(",");
+        score=scoreCurrent.length;
+    }
     document.getElementById("content").style.display="none";
-    document.getElementById("result").innerHTML="<a style='font-size:50px'>"+name+", Your score is "+score+" out of 5.</a>";
+    document.getElementById("result").innerHTML="<a style='font-size:50px'>"+name+", Your score is "+score+" out of 5</a>";
 }
+
